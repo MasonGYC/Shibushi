358d41c1efe22df1200232380ac44190df3eaabd
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2022-03-10 17:05:42 +0800

    create android studio files

---
+++ app/src/main/java/com/example/shibushi/MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.shibushi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+}
+/*
+* This is the Main screen that the user sees. It shows the
+* */
