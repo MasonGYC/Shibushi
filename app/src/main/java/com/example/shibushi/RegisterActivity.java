808610e3b30c1cee03901c4a223087d6ecc3370a
samuel chiang <samjele008@gmail.com>
2022-03-20 11:27:22 +0800

    git checkout -b realtime

---
+++ app/src/main/java/com/example/shibushi/RegisterActivity.java
@@ -0,0 +1,171 @@
+package com.example.shibushi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button bRegister;
+    EditText etEmailAddress, etPassword, etUsername;
+    ProgressBar progressBar;
+    TextView txt_login;
+
+    FirebaseAuth mAuth;
+    DatabaseReference dReference;
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        bRegister = findViewById(R.id.bRegister);
+
+        etEmailAddress = findViewById(R.id.etEmailAddress);
+        etPassword = findViewById(R.id.etPassword);
+        etUsername = findViewById(R.id.etUsername);
+        progressBar = findViewById(R.id.progress_circular);
+        txt_login = findViewById(R.id.txt_login);
+
+        //
+        mAuth = FirebaseAuth.getInstance();
+
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pd = new ProgressDialog(RegisterActivity.this);
+                pd.setMessage("Register");
+                pd.show();
+
+                String email = etEmailAddress.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String username = etUsername.getText().toString().trim();
+                // Check all fields...
+                if (username.isEmpty()||email.isEmpty()||password.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Not all fields are filled...",Toast.LENGTH_SHORT).show();
+                }
+                if(email.isEmpty()){
+                    etEmailAddress.setError("Email address is required!");
+                    etEmailAddress.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    etEmailAddress.setError("Please enter a valid email address!");
+                    etEmailAddress.requestFocus();
+                    return;
+                }
+                //Username validation
+                if (username.isEmpty()){
+                    etUsername.setError("Username is required!");
+                    etUsername.requestFocus();
+                    return;
+                }
+                //Password validation
+                if (password.isEmpty()){
+                    etPassword.setError("Password is required!");
+                    etPassword.requestFocus();
+                    return;
+                }
+                if (password.length()<6){
+                    etPassword.setError("Min password length is 6 characters!");
+                }
+                //Todo check Password strength
+                else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    register(username, email, password);
+                    progressBar.setVisibility(View.GONE);
+                }
+
+
+            }
+        });
+
+    }
+
+    private void register(String username, String email, String password){
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                    String userid = firebaseUser.getUid();
+
+                    dReference = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+
+                    HashMap<String,Object> hashMap = new HashMap<>();
+                    hashMap.put("id", userid);
+                    hashMap.put("username", username.toLowerCase());
+                    hashMap.put("bio", "");
+                    hashMap.put("imageurl","https://firebasestorage.googleapis.com/v0/b/shibushi.appspot.com/o/images%2Fayayaka.png?alt=media&token=56948b63-37c1-432a-83d6-53793e79e1ee" );
+
+                    dReference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                pd.dismiss();
+                                progressBar.setVisibility(View.GONE);
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }
+                else{
+                    pd.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/shibushi/StartActivity.java
@@ -0,0 +1,57 @@
+package com.example.shibushi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button bLogin, bRegister;
+
+    FirebaseUser firebaseUser;
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        // if user already logged in
+        if (firebaseUser != null){
+            startActivity(new Intent(StartActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        bLogin = findViewById(R.id.bLogin);
+        bRegister = findViewById(R.id.bRegister);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+
+
