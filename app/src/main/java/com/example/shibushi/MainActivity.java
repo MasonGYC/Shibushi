2b1b51a0e155539b1f058cd252d20e003fe95129
Mason <yuchen_guo@mymail.sutd.edu.sg>
2022-03-24 20:49:53 +0800

    Add camera button, take picture and store locally

--- app/src/main/java/com/example/shibushi/MainActivity.java
+++ app/src/main/java/com/example/shibushi/MainActivity.java
@@ -1,22 +1,36 @@
 package com.example.shibushi;
 
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button bLogout, bChangePassword, bImportClothing;
+    Button bLogout, bChangePassword, bImportClothing, bTakePhoto;
     TextView tvWelcome;
     String welcome;
     private FirebaseAuth mAuth;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +61,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         tvWelcome = findViewById(R.id.tvWelcome);
         tvWelcome.setText(welcome);
+
+        //take photo
+        bTakePhoto = findViewById(R.id.bTakePhoto);
+        bTakePhoto.setOnClickListener(this);
+
     }
     @Override
     public void onClick(View v){
@@ -60,8 +79,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.bImportClothing:
                 startActivity(new Intent(this, ImportClothing.class));
                 break;
+            case R.id.bTakePhoto:
+                TakePhoto takePhoto = new TakePhoto();
+                dispatchTakePictureIntent(takePhoto);
+                break;
             default:
                 throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    }
+
+    //camera
+    public void dispatchTakePictureIntent(TakePhoto takePhoto) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Log.i("if2","resolveActivity");
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = takePhoto.createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                Log.i("dispatch","if (photoFile != null) ");
+                startActivity(takePictureIntent);
+                takePhoto.galleryAddPic();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap thumbnail = data.getParcelableExtra("data");
+            // Do other work with full size photo saved in locationForPhotos
+
         }
     }
 
@@ -76,6 +137,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "You aren't logged in yet!", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
+
+
+
 
 }
 
---
+++ app/src/main/java/com/example/shibushi/TakePhoto.java
@@ -0,0 +1,58 @@
+package com.example.shibushi;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TakePhoto extends AppCompatActivity {
+
+
+    String currentPhotoPath;
+
+    //store in public Pictures directory
+    public File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Shibushi_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+
+    public void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+        Log.i("add","galleryAddPic");
+    }
+}
